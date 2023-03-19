2dc9cb7708e5b76cd6ecaa9ebc6648b0e248d981
Alex Johnson <alexj0127@gmail.com>
2023-03-19 15:45:24 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const timeBlock = document.getElementById("timeBlock");
+const cityInput = document.getElementById("cityInput");
+const searchButton = document.getElementById("submit");
+const currentWeather = document.querySelector(".currentConditon");
+const curTemp = document.querySelector("#curTemp");
+const curCond = document.getElementById("curCond");
+const wind = document.getElementById("windSpeed");
+const feelsLike = document.getElementById("feelsLike");
+
+const APIKEY = 'c6094e9c05134d49b8e170231231903';
+
+
+window.onload = updateTime;
+setInterval(updateTime, 1000);
+searchButton.addEventListener("click", getWeather);
+
+function updateTime(){
+    let curTime = new Date();
+    let minutes = curTime.getMinutes();
+    let hours = curTime.getHours();
+    let amPm = 'AM';
+    if (hours >= 12){
+        amPm = 'PM';
+        hours = (hours == 12) ? 12 : hours % 12;
+    }
+    minutes = (minutes < 10) ? '0'+minutes : minutes;
+    timeBlock.innerHTML = `${hours}:${minutes} ${amPm}`;
+}
+
+function getWeather(){
+    let city = cityInput.value;
+    let url = `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${city}&aqi=no`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            curTemp.innerText = `${result.current.temp_f} \u00B0F`;
+            curCond.innerText = `${result.current.condition.text}`;
+            wind.innerText = `Wind: ${result.current.wind_mph} MPH`;
+            feelsLike.innerText = `Feels Like: ${result.current.feelslike_f} \u00B0F`;
+        })
+        
+        
+}
