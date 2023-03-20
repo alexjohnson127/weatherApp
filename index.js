456ded7e9342dbccc40843dce7aafb7f0fd739c5
Alex Johnson <alexj0127@gmail.com>
2023-03-19 20:13:21 -0600

    starting to add forecast

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const curTemp = document.querySelector("#curTemp");
 const curCond = document.getElementById("curCond");
 const wind = document.getElementById("windSpeed");
 const feelsLike = document.getElementById("feelsLike");
+const extendedForecast = document.getElementById("extendedForecast");
+const forecastButton = document.getElementById("forecastButton");
 
 const APIKEY = 'c6094e9c05134d49b8e170231231903';
 
@@ -13,6 +15,7 @@ const APIKEY = 'c6094e9c05134d49b8e170231231903';
 window.onload = updateTime;
 setInterval(updateTime, 1000);
 searchButton.addEventListener("click", getWeather);
+forecastButton.addEventListener("click", getForecast);
 
 function updateTime(){
     let curTime = new Date();
@@ -39,7 +42,22 @@ function getWeather(){
             curCond.innerText = `${result.current.condition.text}`;
             wind.innerText = `Wind: ${result.current.wind_mph} MPH`;
             feelsLike.innerText = `Feels Like: ${result.current.feelslike_f} \u00B0F`;
+        }) 
+}
+
+function getForecast(){
+    let city = cityInput.value;
+    let url = `http://api.weatherapi.com/v1/forecast.json?key=${APIKEY}&q=${city}&days=1&aqi=no&alerts=no`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            let forecastData = result;
+            console.log(forecastData);
+            for (let i in forecastData.forecast.forecastday[0].hour){
+                console.log(forecastData.forecast.forecastday[0].i.temp_f);
+            }
         })
-        
-        
+
+    
 }
